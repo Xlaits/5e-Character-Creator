d665018f23b102f76027d304c2a5c2dffbd2cb14
Xlaits Xavier <Andrew.A.Roderigues@gmail.com>
2018-05-07 15:56:19 -0400

    Began coding master header
    Inclided all required headers, and any possible requirements for character generation.

---
+++ character.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include "backgrounds.h"
+#include "classes.h"
+#include "equipment.h"
+#include "mainStats.h"
+#include "races.h"
+#include "skills.h"
+#include "spells.h"
+
+using namespace std;
