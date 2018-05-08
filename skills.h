f29e4eeb8edbf4befe54997a15d4c3c79adfdde7
Xlaits <WhiteTigerTails@gmail.com>
2018-05-07 21:27:26 -0400

    Setting up more of skills.

--- skills.h
+++ skills.h
@@ -8,5 +8,11 @@ class Skills()
 private:
 	const string langs[15] = {"Common", "Dwarvish", "Elvish", "Giant", "Gnomish", "Goblin", "Halfling", "Orc", "Abyssal", "Celestial", "Draconic", "Deep Speech", "Infernal", "Primordial", "Sylvan", "Undercommon"};
 	
-	const string raceSkills[] = {"Darkvision", "Dwarven Resilience", "Dwarven Combat Training", "Dwarven Tool Proficiency", "Stonecunning", "Dwarven Toughness", "Keen Senses", "Fey Ancestry", "Trance", "High Elf Weapon Training", "Eood Elf Weapon Training", "Fleet of Foot", "Mask of the Wild", "Superior Darkvision", "Sunlight Sensitivity", "Drow Magic", "Drow Weapon Training", "Lucky", "Brave", "Halfling Nimbleness", "Naturally Stealthy", "Stout Resilience", "Human Variant", "Draconic Ancestry", "Breath Weapon", "Draconic Resistance", }
+	const string raceSkills[37] = {"Darkvision", "Dwarven Resilience", "Dwarven Combat Training", "Dwarven Tool Proficiency", "Stonecunning", "Dwarven Toughness", "Keen Senses", "Fey Ancestry", "Trance", "High Elf Weapon Training", "Eood Elf Weapon Training", "Fleet of Foot", "Mask of the Wild", "Superior Darkvision", "Sunlight Sensitivity", "Drow Magic", "Drow Weapon Training", "Lucky", "Brave", "Halfling Nimbleness", "Naturally Stealthy", "Stout Resilience", "Human Variant", "Draconic Ancestry", "Breath Weapon", "Draconic Resistance", "Gnome Cunning", "Natural Illusionist", "Speak with Small Beasts", "Artificer's Lore", "Tinker", "Skill Versatility", "Menacing", "Relentless Endurance", "Savage Attacks", "Hellish Resistance", "Infernal Legacy"}
+
+public:
+	Skills()
+	{
+		//Nothing needs to be set, this is just a place to hold all the needed skills.
+	}
 }
